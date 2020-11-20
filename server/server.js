945ef385f72d55be28b2479a565e8cb964280cbd
Kousik Mandal <kousik348@gmail.com>
2020-11-20 07:54:26 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket)=> {
+      console.log('User Online');
+
+      socket.on('canvas-data', (data)=> {
+            socket.broadcast.emit('canvas-data', data);
+            
+      })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
---
+++ ui/whiteboard-collab/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Container from './components/container/Container';
+
+function App() {
+  return (
+    <Container/>
+  );
+}
+
+export default App;
